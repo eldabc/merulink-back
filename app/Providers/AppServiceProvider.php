6d727badead7e37f6cdd421e913dae6f492bd4c0
Elda Bello <elda822@gmail.com>
2026-04-30 16:45:32 -0400

    Fix en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Relation::enforceMorphMap([
+            'user'     => 'App\Models\User',
             'employee' => 'App\Models\Employee',
             'event_contact'  => 'App\Models\EventContact',
         ]);
