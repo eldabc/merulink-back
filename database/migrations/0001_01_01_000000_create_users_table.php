e0cb5d1d0b339642806de38678e0774013325768
Elda Bello <elda822@gmail.com>
2026-07-14 17:01:02 -0400

    Eliminando campos name, email,            email_verified_at  de la tabla users.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            // $table->string('name');
             $table->string('username')->unique();
-            $table->string('email')->unique()->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->string('email')->unique()->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('status')->default(true)->after('password');
             $table->rememberToken();
